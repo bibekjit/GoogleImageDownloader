ee7e58aec4620993490c0c3995f5888e2871749b
bibekjit <77575222+bibekjit@users.noreply.github.com>
2022-01-31 16:42:03 +0530

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,44 +1,66 @@
-"""
-Before running the code, make sure you have installed the selenium webdriver for your browser.
-Go to - https://selenium-python.readthedocs.io/installation.html -  to download and install the webdriver
-"""
-
 from selenium import webdriver
-import urllib.request
-from PIL import Image
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from urllib.request import urlretrieve
+import time
 import os
 
-keyword = input('enter keyword : ')
-n = int(input('how many images ? '))
-url = input('enter url : ')
-size = int(input('enter image size : '))
-path = input('enter path to directory to save the images : ')
-driver_path = input('enter path to webdriver : ') 
+QUERY=input("Search Image : ")
+IMAGE_RESOLUTION=input("Image Resolution : ")
+NUMBER_OF_IMAGES=int(input("How many? : "))
 
-driver=webdriver.Chrome('E://Old//card//chromedriver.exe')
-driver.get(url)
+WAIT=1
+SAVE_DIR=f"{query.replace(" ","_")}"
 
-i=1
-j=1
-while j<=n:
+def download(query, img_num, save_dir, img_res, wait):
+    """
+    auto downloads multiple images from google
+    
+    :param query: image search
+    :param save_dir: image download location
+    :param img_res: image resolution
+                    by default medium (medium -> m)
+                    large -> l ; small -> i
+    :param wait: wait time, for loading the images, in seconds. 
+                            varies as per internet speed (default value = 1)
+                            increase the value if internet speed is slow
+                            
+    
+    """
+    
+    # create save directory if not there
+    if save_dir not in os.listdir():
+        os.mkdir(save_dir)
+    
+    query = '%20'.join(query.split())
+    
+    # initiate webdriver
+    s = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=s)
 
-    try:
-        driver.find_element_by_xpath('//*[@id="islrg"]/div[1]/div[{}]/a[1]/div[1]/img'.format(i)).click()
-        img=driver.find_element_by_xpath('//*[@id="Sva75c"]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div/div[2]/a/img')
-        link=img.get_attribute('src')
-        urllib.request.urlretrieve(link,path+'//'+keyword+' '+str(i)+'.jpg')
+    url = f'https://www.google.co.in/search?q={query}&tbm=isch&hl=en&tbs=isz:{img_res}&authuser=0&sa=X&ved=0CAIQpwVqFwoTCIiRwOri1vUCFQAAAAAdAAAAABAC&biw=1519&bih=754'
+    driver.get(url)
+    driver.maximize_window()
+    time.sleep(5)
+    
+    # scrape through images and download them
+    for i in range(img_num):
+        try:
+            element = driver.find_element('xpath',
+                                          f'/html/body/div[2]/c-wiz/div[3]/div[1]/div/div/div/div[1]/div[1]/span/div[1]/div[1]/div[{i + 1}]/a[1]/div[1]/img')
+            element.click()
+            time.sleep(wait)
+            image = driver.find_element('xpath',
+                                        '/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[2]/div/a/img')
+            img_src = image.get_attribute('src')
+            urlretrieve(img_src, save_dir + "\\" + str(img_num) + '.jpg')
+        except:
+            pass
 
-        im=Image.open(path+'//'+keyword+' '+str(i)+'.jpg')
-
-        if im.size[0]<size and im.size[1]<size :
-            im.close()
-            os.remove(path+'//'+keyword+' '+str(i)+'.jpg')
-        else:
-            print(j,keyword+' '+str(i)+'.jpg',(im.size[0],im.size[1]))
-            j+=1
-        i+=1
+download(QUERY,NUM_OF_IMAGES,SAVE_DIR,IMAGE_RESOLUTION,WAIT)
 
 
-    except:
-        i+=1
-        pass
+
+
+
+
