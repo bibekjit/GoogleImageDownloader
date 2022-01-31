42280df98f326b0f74c0d36282ce5151d0c87b17
bibekjit <77575222+bibekjit@users.noreply.github.com>
2022-01-31 17:42:59 +0530

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,66 +1,85 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 from urllib.request import urlretrieve
+from pyvirtualdisplay import Display
 import time
 import os
 
-QUERY=input("Search Image : ")
-IMAGE_RESOLUTION=input("Image Resolution : ")
-NUMBER_OF_IMAGES=int(input("How many? : "))
+QUERY = input("Search Image : ")
+IMAGE_RESOLUTION = input("Image Resolution : ")
+NUMBER_OF_IMAGES = int(input("How many? : "))
 
-WAIT=1
-SAVE_DIR=f"{query.replace(" ","_")}_{NUMBER_OF_IMAGES}"
+# set these according to internet speed
+WAIT_TIME_PER_IMAGE = 2
+INITIAL_WAIT_TIME = 5
 
-def download(query, img_num, save_dir, img_res, wait):
+
+HEADLESS=True
+
+SAVE_DIR = f"{QUERY.replace(' ','_')}_{NUMBER_OF_IMAGES}"
+
+
+def download(query, img_num, img_res):
     """
-    auto downloads multiple images from google
-    
+    Auto downloads multiple images from google
+
     :param query: image search
     :param save_dir: image download location
     :param img_res: image resolution
                     by default medium (medium -> m)
                     large -> l ; small -> i
-    :param wait: wait time, for loading the images, in seconds. 
-                            varies as per internet speed (default value = 1)
-                            increase the value if internet speed is slow
-                            
-    
     """
-    
+
+    global WAIT_TIME_PER_IMAGE
+    global INITIAL_WAIT_TIME
+    global SAVE_DIR
+    global HEADLESS
+
     # create save directory if not there
-    if save_dir not in os.listdir():
-        os.mkdir(save_dir)
-    
+    if SAVE_DIR not in os.listdir():
+        os.mkdir(SAVE_DIR)
+
     query = '%20'.join(query.split())
+
     
+
     # initiate webdriver
     s = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=s)
+    
+    
+    if HEADLESS: # hides webdriver browser window
+        options = Options()
+        options.add_argument("--headless")
+        driver = webdriver.Chrome(service=s,options=options)
+        
+    else: # set HEADLESS as False otherwise
+        driver = webdriver.Chrome(service=s)
+
+
 
     url = f'https://www.google.co.in/search?q={query}&tbm=isch&hl=en&tbs=isz:{img_res}&authuser=0&sa=X&ved=0CAIQpwVqFwoTCIiRwOri1vUCFQAAAAAdAAAAABAC&biw=1519&bih=754'
     driver.get(url)
     driver.maximize_window()
-    time.sleep(5)
-    
+    time.sleep(INITIAL_WAIT_TIME)
+
+    print("\ndownloading...")
     # scrape through images and download them
     for i in range(img_num):
         try:
             element = driver.find_element('xpath',
                                           f'/html/body/div[2]/c-wiz/div[3]/div[1]/div/div/div/div[1]/div[1]/span/div[1]/div[1]/div[{i + 1}]/a[1]/div[1]/img')
             element.click()
-            time.sleep(wait)
+            time.sleep(WAIT_TIME_PER_IMAGE)
             image = driver.find_element('xpath',
                                         '/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[2]/div/a/img')
             img_src = image.get_attribute('src')
-            urlretrieve(img_src, save_dir + "\\" + str(img_num) + '.jpg')
+            urlretrieve(img_src, SAVE_DIR + "\\" + str(i) + '.jpg')
         except:
             pass
 
-download(QUERY,NUM_OF_IMAGES,SAVE_DIR,IMAGE_RESOLUTION,WAIT)
 
+    print(f"\n\nDOWNLOADED {len(os.listdir(SAVE_DIR))}/{img_num} IMAGES")
 
-
-
-
-
+download(QUERY, NUMBER_OF_IMAGES, IMAGE_RESOLUTION)
